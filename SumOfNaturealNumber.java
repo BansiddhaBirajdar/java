12ec667c9c647e8785262c544d2dda19b2b7f789
BansiddhaBirajdar <60480136+BansiddhaBirajdar@users.noreply.github.com>
2021-05-24 21:05:11 +0530

    Create SumOfNaturealNumber.java

---
+++ SumOfNaturealNumber.java
@@ -0,0 +1,17 @@
+
+public class SumOfNaturealNumber{
+
+     public static void main(String []args){
+        int ino=1;
+        // with loop
+        // int sum=0;
+        // while(ino>0){
+        //     sum+=ino;
+        //     ino--;
+        // }
+        // System.out.printf("Sum of %d ",sum);
+        // with out loop
+         System.out.printf("Sum of %d ",((ino*(ino+1))/2));
+
+     }
+}
