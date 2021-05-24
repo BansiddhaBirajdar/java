06b9ccd8d6a0445b83f6364b4732c29f4efc72fb
BansiddhaBirajdar <60480136+BansiddhaBirajdar@users.noreply.github.com>
2021-05-24 08:53:17 +0530

    Update FindSecondSmallestNumber.java

--- FindSecondSmallestNumber.java
+++ FindSecondSmallestNumber.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 class FindSecondSmallestNumber {
     public static void main(String[] args) {
         int []arr =new int[]{-1,2,10,0,5,11,15};
-        /*int small=Integer.MAX_VALUE;
+        int small=Integer.MAX_VALUE;
         int secondSmall=Integer.MAX_VALUE;
         for(int value:arr){
             if(value<small){
@@ -18,6 +18,5 @@ class FindSecondSmallestNumber {
         // first way with sorting find the second smallest number
         // Arrays.sort(arr);
         // System.out.printf("Second smallest number is %d",arr[1]);
-        */
     }
 }
